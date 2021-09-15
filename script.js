2158bfda67052393ad0842d7f30db7b518eac250
SecretsOfDeat <secretofdeath00@gmail.com>
2021-09-15 00:12:22 -0500

    new

---
+++ script.js
@@ -0,0 +1,9 @@
+//Desabilitar click derecho en la pagina completa
+$("body").on("contextmenu", function(e){
+    return false;
+});
+
+//Desabilitar click derecho una parte
+//$(".ctn-p img").on("contextmenu", function(e){
+//    return false;
+//});
